cb660467811fe609f11bd2f3e3d6eb527b641050
Mihir Santosh Sanjay <exit>
2025-09-23 13:19:48 -0500

    adding highlights and symbols for sorting

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,13 +1,24 @@
 module MoviesHelper
-  def toggle_direction(column)
-    # Use params first, fallback to session
+  def sorted_class(column)
     current_sort = params[:sort] || session[:sort] || "title"
     current_direction = params[:direction] || session[:direction] || "asc"
 
     if current_sort == column
-      current_direction == "asc" ? "desc" : "asc"
+      current_direction == "asc" ? "sorted-asc" : "sorted-desc"
     else
-      "asc"
+      ""
+    end
+  end
+
+
+  def sorted_symbol(column)
+    current_sort = params[:sort] || session[:sort] || "title"
+    current_direction = params[:direction] || session[:direction] || "asc"
+
+    if current_sort == column
+      current_direction == "asc" ? "▲" : "▼" # Up or down arrow
+    else
+      ""
     end
   end
 end
